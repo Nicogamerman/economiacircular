e23a855fb0a70b7199e846eb4627f6e17f5e7501
Santiago Ibarbia <santibarbia@gmail.com>
2025-07-21 20:37:52 -0300

    fix cors

--- src/main/java/com/pp/economia_circular/config/SecurityConfig.java
+++ src/main/java/com/pp/economia_circular/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http    .cors()
+                .and()
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
