d1e9888b92d6cedd08c1fb94be841cef9eff7ee0
Santiago Ibarbia <santibarbia@gmail.com>
2025-07-21 20:48:27 -0300

    fix cors

--- src/main/java/com/pp/economia_circular/EconomiaCircularApplication.java
+++ src/main/java/com/pp/economia_circular/EconomiaCircularApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.pp.economia_circular")
+@ComponentScan(basePackages = {"com.pp.economia_circular","com.pp.economia_circular.config" })
 @EnableJpaRepositories(basePackages = "com.pp.economia_circular.repositories")
 public class EconomiaCircularApplication {
 
---
+++ src/main/java/com/pp/economia_circular/config/WebConfiguration.java
@@ -0,0 +1,27 @@
+package com.pp.economia_circular.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));  // Permite todos los orígenes
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(false); // Importante: con "*" no se permite true en allowCredentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
