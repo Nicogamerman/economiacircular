85d65086bce74d4e4f55953e67023434dbccb6c8
Santiago Ibarbia <santibarbia@gmail.com>
2025-08-27 20:22:52 -0300

    health check

---
+++ src/main/java/com/pp/economia_circular/controller/PingController.java
@@ -0,0 +1,13 @@
+package com.pp.economia_circular.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+        @GetMapping("/")
+        public String health() {
+            return "OK";
+        }
+
+}
